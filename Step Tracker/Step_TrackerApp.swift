efafd86f4c6dc258b3ff1266fa5558901da7444d
jeffSweeney <92553377+jeffSweeney@users.noreply.github.com>
2024-06-11 18:16:33 -0400

    Initial app creation

---
+++ Step Tracker/Step_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_TrackerApp.swift
+//  Step Tracker
+//
+//  Created by Jeffrey Sweeney on 6/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Step_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
